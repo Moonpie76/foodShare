3b2cf97cd9854d0b44329a772603379ead73bb50
Moonpie76 <cs20000706@163.com>
2021-05-07 18:07:02 +0800

    --story=1005015 --user=陈爽 自动定位与手动选择城市 https://www.tapd.cn/38749195/s/1005516

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -4,14 +4,16 @@ Page({
    * 页面的初始数据
    */
   data: {
-    
+    city: '',
+    cityPickerValue: [0, 0],
+    cityPickerIsShow: false,
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    
+    this.autoLocate()
   },
 
   /**
@@ -61,5 +63,70 @@ Page({
    */
   onShareAppMessage: function () {
     
+  },
+
+  /**
+   * 城市选择确认
+   */
+  cityPickerOnSureClick: function (e) {
+    console.log('cityPickerOnSureClick');
+    console.log(e);
+    this.setData({
+      city: e.detail.valueName[1],
+      cityPickerValue: e.detail.valueCode,
+      cityPickerIsShow: false,
+    });
+
+  },
+  /**
+   * 城市选择取消
+   */
+  cityPickerOnCancelClick: function (event) {
+    console.log('cityPickerOnCancelClick');
+    console.log(event);
+    this.setData({
+      cityPickerIsShow: false,
+    });
+  },
+
+
+  showCityPicker() {
+    // this.data.cityPicker.show()
+    console.log("show city-picker")
+    this.setData({
+      cityPickerIsShow: true,
+    });
+  },
+
+  autoLocate: function () {
+    var page = this
+    wx.getLocation({
+     type: 'wgs84',
+     success: function (res) {
+     // success 
+     var longitude = res.longitude
+     var latitude = res.latitude
+     console.log(longitude)
+     page.loadCity(longitude, latitude)
+     }
+    })
+  },
+     
+    loadCity: function (longitude, latitude) {
+      var page = this
+      wx.request({
+      url: 'https://api.map.baidu.com/reverse_geocoding/v3/?ak=zFlHvfuzeORso0OveUQDCElE118kdlbz&output=json&coordtype=wgs84ll&location=' + latitude + ',' + longitude + '',
+      data: {},
+      header: {
+        'Content-Type': 'application/json'
+      },
+     success: function (res) {
+     // success 
+     console.log(res);
+     var city = res.data.result.addressComponent.city;
+     console.log("城市为" + city)
+     page.setData({ city: city });
+     }
+    })
   }
 })
