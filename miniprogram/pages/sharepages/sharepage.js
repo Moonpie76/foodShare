cf21d947c52bdd936ec127577653fb5825578783
ladyluck12 <66951898+ladyluck12@users.noreply.github.com>
2021-05-13 10:29:33 +0800

    --story=1004191 --user=小麦 上传图片 https://www.tapd.cn/38749195/s/1005811

--- miniprogram/pages/sharepages/sharepage.js
+++ miniprogram/pages/sharepages/sharepage.js
@@ -15,7 +15,11 @@ Page({
     cityPickerIsShow: false,
    // fileIDs: {},
     //testa: [],
+<<<<<<< HEAD
     testb: [],
+=======
+    //testb: [],
+>>>>>>> 3b24eee2b50c515ac1681f3f05df8bea73c2a93f
     Astring: [],
     imageIDs:[],
     time: [],
@@ -179,12 +183,20 @@ uploaddata:async function(e){
     url: '/pages/index/index',
   });
   var that=this;
+  var m=Math.floor(Math.random()*1000000);
+  var n="cloud://cloud1-3g2crjv629049c8c.636c-cloud1-3g2crjv629049c8c-1305648382/"
   for(var i=0;i<that.data.images.length;i++){
     wx.cloud.uploadFile({
+<<<<<<< HEAD
       cloudPath: 'test1/'+ Math.floor(Math.random()*1000000),
       filePath:that.data.images[i],
       success(res){
         console.log(res.fileID)
+=======
+      cloudPath: 'test1/'+ m,
+      filePath:that.data.images[i],
+      success(res){
+>>>>>>> 3b24eee2b50c515ac1681f3f05df8bea73c2a93f
         that.setData({
           Astring:that.data.Astring.concat(res.fileID), 
         });
